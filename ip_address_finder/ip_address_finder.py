ffc4ef92601d979c2779fabc98f5f1b73ab3cac8
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-06-20 12:42:54 -0500

    cleaned up formatting

--- ip_address_finder/ip_address_finder.py
+++ ip_address_finder/ip_address_finder.py
@@ -3,8 +3,7 @@
 import re
 
 def IPAddressCarver(input):
-    return(re.findall(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)',
-   )        input))
+    return(re.findall(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', input))
 
 def getIPAddresses(fileName):
     with open(fileName, 'r') as inputFile:
