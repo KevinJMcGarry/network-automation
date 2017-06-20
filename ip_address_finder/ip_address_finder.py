563ec584af06f5d46b66d17a4c4f76f39874cbf5
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-06-20 12:40:51 -0500

    initial commit

---
+++ ip_address_finder/ip_address_finder.py
@@ -0,0 +1,21 @@
+# Function for carving ip addresses out of a file.
+
+import re
+
+def IPAddressCarver(input):
+    return(re.findall(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)',
+   )        input))
+
+def getIPAddresses(fileName):
+    with open(fileName, 'r') as inputFile:
+        for line in inputFile:
+            found_ips = IPAddressCarver(line)
+            ipAddressList.append(found_ips)
+
+ipAddressList = []
+
+fileName = input('Enter the name of the file you wish to search for IP Addresses: ')
+
+getIPAddresses(fileName)
+
+print(ipAddressList
